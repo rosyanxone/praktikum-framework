f34d27200465ab9ed780be1e111401533c5971c1
tegarfn <tegarfitrah.na@gmail.com>
2023-10-04 22:21:15 +0800

    adding table on mahasiswa's page

--- resources/views/staff/mahasiswa.blade.php
+++ resources/views/staff/mahasiswa.blade.php
@@ -9,6 +9,63 @@ Data Mahasiswa - Staff
     <div class="w-full h-screen flex flex-col bg-slate-100">
         @include('components.header', ['nama' => 'Tegar', 'gambar' => 'assets/images/ka-prodi.jpg'])
 
+        <div class="h-full m-4 p-8 bg-white rounded-lg drop-shadow-md">
+            <p class="text-4xl font-bold mb-4">Data Mahasiswa</p><hr><br>
+            <div class="w-full h-auto flex justify-end">
+                <button class="px-4 py-2 bg-green-600 rounded-md text text-white">Tambah</button>
+            </div><br>
+            <div class="relative overflow-x-auto">
+                <table class="w-full text-sm text-left text-gray-500">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                No.
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                NIM
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Nama
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Kelas
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Angkatan
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Aksi
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                1
+                            </th>
+                            <td class="px-6 py-4">
+                                2009106085
+                            </td>
+                            <td class="px-6 py-4">
+                                Tegar Fitrah Naba Atthoriq
+                            </td>
+                            <td class="px-6 py-4">
+                                B
+                            </td>
+                            <td class="px-6 py-4">
+                                2020
+                            </td>
+                            <td class="px-6 py-4">
+                                <div class="w-full h-auto">
+                                    <button class="px-4 py-2 bg-yellow-300 rounded-md text">Edit</button>
+                                    <button class="px-4 py-2 bg-red-600 rounded-md text text-white">Hapus</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
